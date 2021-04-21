4977329d5efa1ba827aac5b6a6e55a94f73e2c4c
boykis82 <boykis82@gmail.com>
2021-04-21 15:22:33 +0900

    2

--- src/test/java/org/caltech/miniswingpilot/MiniswingPilotApplicationTests.java
+++ src/test/java/org/caltech/miniswingpilot/MiniswingPilotApplicationTests.java
@@ -3,6 +3,8 @@ package org.caltech.miniswingpilot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class MiniswingPilotApplicationTests {
 
@@ -10,4 +12,8 @@ class MiniswingPilotApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void test() {
+		assertThat(1).isEqualTo(1);
+	}
 }
